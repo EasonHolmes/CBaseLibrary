2f8e684a366b5fcdc8858eb9dd2e04c84b1dc1fc
cuiyang <holmeseason@gmail.com>
2021-07-31 16:18:38 +0800

    test2

--- app/src/main/java/com/ethan/clibrary/MainActivity.kt
+++ app/src/main/java/com/ethan/clibrary/MainActivity.kt
@@ -2,7 +2,6 @@ package com.ethan.clibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
