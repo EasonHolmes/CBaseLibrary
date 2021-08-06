5fed0ae6d95c5d22afd7472d37afbed13728e3df
cuiyang <holmeseason@gmail.com>
2021-08-06 18:17:30 +0800

    temp

--- app/src/main/java/com/ethan/clibrary/MainActivity.kt
+++ app/src/main/java/com/ethan/clibrary/MainActivity.kt
@@ -7,13 +7,12 @@ import com.scwang.smart.refresh.layout.SmartRefreshLayout
 import com.utils.library.helper.ActivityStackManager
 import com.utils.library.utils.CCLogUtils
 import com.widget.library.anim.coordinator_anim.CommonBehavior
+import kotlinx.coroutines.flow.MutableSharedFlow
 import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        CCLogUtils
-        ActivityStackManager.getInstance().application
     }
 }
