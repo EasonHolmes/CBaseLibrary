389d122d43a5ca9ff54f48900648ed94e5f8499a
Ethan(崔洋) <ethan.cui@yeahmobi.com>
2022-08-05 17:40:13 +0800

    update

--- app/src/main/java/com/ethan/clibrary/activity/MotionLayoutActivity.kt
+++ app/src/main/java/com/ethan/clibrary/activity/MotionLayoutActivity.kt
@@ -1,7 +1,12 @@
 package com.ethan.clibrary.activity
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.transition.Transition
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.ethan.clibrary.R
 
 /**
@@ -10,6 +15,28 @@ import com.ethan.clibrary.R
 class MotionLayoutActivity : AppCompatActivity(R.layout.activity_motion_layout) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val imageview = findViewById<ImageView>(R.id.imageView2)
+        val anim = ObjectAnimator.ofFloat(imageview,"translationY",-100f,100f).apply {
+            duration = 1000L
+            repeatMode = ValueAnimator.REVERSE
+            repeatCount = ValueAnimator.INFINITE
+        }
+        anim.start()
+        findViewById<MotionLayout>(R.id.constraintlayout).setTransitionListener(object :
+            MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                anim.cancel()
+            }
 
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+        })
     }
 }
